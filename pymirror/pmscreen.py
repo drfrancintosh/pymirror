d041fdfc0ff8935fd5cc62e397e6efd41bce1bf9
Gregory Smith <greg@agilefrontiers.com>
2025-06-24 01:06:49 -0400

    update

--- pymirror/pmscreen.py
+++ pymirror/pmscreen.py
@@ -23,11 +23,12 @@ def _color(t):
     if t is None: return None
     r, g, b = t
     r = (r >> 3) & 0x1F  # Convert to 5 bits
-    g = (g >> 2) & 0x3F  # Convert to 6 bits
+    g0 = (g >> 2) & 0x07  # Convert to lower 3 bits
+    g1 = (g >> 5) & 0x07  # Convert to upper 3 bits
     b = (b >> 3) & 0x1F  # Convert to 5 bits
     if r < 0 or r > 31 or g < 0 or g > 63 or b < 0 or b > 31:
         raise ValueError(f"Invalid color value: {t}, expected RGB tuple with R in [0,31], G in [0,63], B in [0,31].")
-    x = r << 19 | g << 13 | b << 7
+    x = r << 19 | (g1 << 16 | g0 << 5 | b)
     return x
 
 class PMScreen:
