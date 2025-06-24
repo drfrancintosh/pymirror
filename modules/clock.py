7bde7e2b303554452e1e05e349110c7a66b43c96
Gregory Smith <greg@agilefrontiers.com>
2025-06-23 20:35:54 -0400

    update

--- modules/clock.py
+++ modules/clock.py
@@ -5,17 +5,14 @@ from pymirror.pmscreen import PMGfx
 class Clock(PMModule):
 	def __init__(self, pm, moddef, config):
 		super().__init__(pm, moddef, config)
-		if not self.gfx.font_name: self.gfx.font_name = "DejaVuSans"
-		if not self.gfx.font_size: self.gfx.font_size = 32
-		self.gfx.set_font(self.gfx.font_name, self.gfx.font_size)
 		self.date_format = "%I:%M:%S %p"
 		if config.date_format: self.date_format = config.date_format
 
 	def render(self):
 		now = datetime.now()
-		formatted = now.strftime(self.date_format)
+		date_str = now.strftime(self.date_format)
 		gfx = self.gfx
-		self.screen.text_box(gfx, formatted,
+		self.screen.text_box(gfx, date_str,
 			gfx.x0 + self.x_offset, gfx.y0 + self.y_offset,
 			gfx.x1 + self.x_offset, gfx.y1 + self.y_offset)
 
