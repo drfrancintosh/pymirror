ea3460675f28484f70f93f05ae08b2b956f6b96f
Gregory Smith <greg@agilefrontiers.com>
2025-06-24 00:05:50 -0400

    update

---
+++ modules/rainbow.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from pymirror.pmmodule import PMModule
+from pymirror.pmscreen import PMGfx
+
+class Rainbow(PMModule):
+	def __init__(self, pm, moddef, config):
+		super().__init__(pm, moddef, config)
+	
+	def render(self):
+		gfx = self.gfx
+		x = gfx.x0
+		y = gfx.y0
+		width = gfx.x1 - gfx.x0
+		height = gfx.y1 - gfx.y0
+		color = 0
+		max = x * y
+		for i in range(max):
+			gfx.color = (color % 256, (color * 2) % 256, (color * 3) % 256)
+			gfx.fill_rect(x + (i % width), y + (i // width), 1, 1)
+			color += 1
+
+	def exec(self):
+		self.render()
+
+	def onEvent(self, event):
+		pass
+
