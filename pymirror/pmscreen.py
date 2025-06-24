ed32f051be1bd1e15617db9b485d50bfa690db26
Gregory Smith <greg@agilefrontiers.com>
2025-06-23 23:56:43 -0400

    update

--- pymirror/pmscreen.py
+++ pymirror/pmscreen.py
@@ -6,7 +6,7 @@ from pymirror.pmgfx import PMGfx
 def _image_to_rgb565(img):
     """Convert a Pillow RGB image to raw RGB565 bytes using numpy"""
     # Convert image to RGB (ensure no alpha channel) and then to numpy array
-    # img = img.convert("RGB")  # Make sure the image is in RGB mode
+    img = img.convert("raw", "BGR;16")  # Make sure the image is in RGB mode
     img_array = np.array(img, dtype=np.uint16)  # Convert to uint16 for correct shifting
     return img_array
     # Perform the conversion to RGB565 for all pixels at once
