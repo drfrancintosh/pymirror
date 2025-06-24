219c87fdde5772563cbddf62e57aa068bc3b9907
Gregory Smith <greg@agilefrontiers.com>
2025-06-24 03:29:32 -0400

    update

--- modules/weather.py
+++ modules/weather.py
@@ -34,7 +34,7 @@ class OpenWeatherMap:
 class Weather(PMModule):
 	def __init__(self, pm, moddef, config):
 		super().__init__(pm, moddef, config)
-		self.weather_data = WeatherData(**moddef.config.__dict__)
+		self.weather_data = WeatherData(**moddef.config.weather_data)
 		self.refresh_minutes = 5
 		self.set_timeout(1) # refresh right away
 		self.weather_response = None
