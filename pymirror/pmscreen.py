2eb6f54bdd14eca168241566aa27b07cea145253
Gregory Smith <greg@agilefrontiers.com>
2025-06-24 00:51:02 -0400

    update

--- pymirror/pmscreen.py
+++ pymirror/pmscreen.py
@@ -25,7 +25,7 @@ def _color(t):
     r = (r >> 3) & 0x1F  # Convert to 5 bits
     g = (g >> 2) & 0x3F  # Convert to 6 bits
     b = (b >> 3) & 0x1F  # Convert to 5 bits
-    x = 0x001f0000
+    x = 0x001f00
     return x
 
 class PMScreen:
