d3a82eaf436eb5efab17f9ffd356096c9b535302
Gregory Smith <greg@agilefrontiers.com>
2025-06-24 22:17:18 -0400

    update

--- modules/fps.py
+++ modules/fps.py
@@ -7,18 +7,16 @@ class Fps(PMModule):
 		self.last_time = datetime.now()
 
 	def render(self):
-		pass
-
-	def exec(self):
-		self.gfx.font_size = 32
-		self.gfx.reset_font()
 		now = datetime.now()
 		delta = now - self.last_time
 		self.last_time = now
 		fps = 1 / delta.total_seconds() if delta.total_seconds() > 0 else 0
 		text_box = self.screen.text_box
-		text_box(self.gfx, f"FPS: {fps:.2f}", self.gfx.x0, self.gfx.y0, valign=self.config.valign, halign=self.config.halign)
+		text_box(self.gfx, f"FPS: {fps:.2f}", self.gfx.rect, valign=self.config.valign, halign=self.config.halign)
 		return 1
+
+	def exec(self):
+		return True
 
 	def onEvent(self, event):
 		pass			
