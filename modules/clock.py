dfea86dde8a93fa9bfcd7a0449c21b17729f0e1f
Gregory Smith <greg@agilefrontiers.com>
2025-06-24 21:12:29 -0400

    update

--- modules/clock.py
+++ modules/clock.py
@@ -14,8 +14,8 @@ class Clock(PMModule):
 		gfx = self.gfx
 		gfx.text_bg_color = gfx.bg_color
 		self.screen.text_box(gfx, self.curr_time,
-			gfx.x0 + self.moddef.x_offset, gfx.y0 + self.moddef.y_offset,
-			gfx.x1 + self.moddef.x_offset, gfx.y1 + self.moddef.y_offset)
+			(gfx.x0 + self.moddef.x_offset, gfx.y0 + self.moddef.y_offset,
+			gfx.x1 + self.moddef.x_offset, gfx.y1 + self.moddef.y_offset))
 		return 1
 
 	def exec(self):
