16ff0bb7aa6fa32bf81a24a1f38d66281bbdf850
Gregory Smith <greg@agilefrontiers.com>
2025-06-26 12:53:15 -0400

    update

--- modules/webapi.py
+++ modules/webapi.py
@@ -9,14 +9,15 @@ from types import SimpleNamespace
 from dataclasses import dataclass
 from pymirror.pmmodule import PMModule
 from modules.alert import AlertEvent
+from pymirror.utils import expand_dict
 
-class WebApi:
+class Api:
 	def __init__(self, config):
 		self.config = config
 
 	def fetch(self):
 
-		response = requests.get(self.base_url, params=args.__dict__)
+		response = requests.get(self.config.url, params=self.config.params.__dict__)
 		if response.ok:
 			return response.json()
 		else:
@@ -32,47 +33,28 @@ def _paragraph_fix(text: str) -> str:
 		results.append(paragraph)
 	return "\n\n".join(results)
 
-class Weather(PMModule):
+class WebApi(PMModule):
 	def __init__(self, pm, moddef, config):
 		super().__init__(pm, moddef, config)
-		self.weather_data = WeatherData(**config.weather_data.__dict__)
-		self.weather_data.appid = os.path.expandvars(self.weather_data.appid) # Expand environment variables
-		self.refresh_minutes = 5
+		self.api = Api(config)
 		self.set_timeout(1) # refresh right away
-		self.weather_response = None
-		self.weather_api = OpenWeatherMap()
 
 	def render(self, force: bool = False) -> int:
-		if not self.weather_response: return 0
-		gfx = copy.copy(self.gfx)
-		degrees = "\u00B0F"  # Degree symbol for Fahrenheit
-		x = gfx.x0
-		y = gfx.y0
-		w = SimpleNamespace(**self.weather_response.get("current"))
-		if not w: return 0
-		text = self.screen.text
-		text_box = self.screen.text_box
-    	# text_box(self, gfx: PMGfx, msg: str, rect: tuple, valign: str = "center", halign: str = "center") -> None:
-		# text(gfx, f"Temp: {w.temp}F\nHumidity: {w.humidity}\nFeels Like: {w.feels_like}F\n{w.weather[0].descripition}", x, y)
-		text_box(gfx, "Weather", gfx.rect, valign="top")
-		y += gfx.font_size / 2  # Offset for the text
-		gfx.text_color = self.config.body_color or gfx.text_color
-		text_box(gfx, f"{w.temp}{degrees}", (x, y + gfx.font_size, gfx.x1, y + gfx.font_size * 2))
-		text_box(gfx, f"{w.humidity} %", (x, y + gfx.font_size * 2, gfx.x1, y + gfx.font_size * 3))
-		text_box(gfx, f"{w.feels_like}{degrees}", (x, y + gfx.font_size * 3, gfx.x1, y + gfx.font_size * 4))
-		self.weather_response = None  # Clear response after rendering
-		return 1
+		context = {
+			"_n_": 0,
+			"payload": self.response,
+		}
+		display = copy.copy(self.config.display.__dict__)
+		display = expand_dict(self.response, context)
+		print(f"WebApi.render: {display}")
 
 	def exec(self) -> bool:
 		if not self.is_timedout(): return False
-		self.weather_response = self.weather_api.fetch(self.weather_data)
-		self.set_timeout(self.refresh_minutes * 60 * 1000)
-		if self.weather_response.get("alerts"):
-			alerts = self.weather_response["alerts"]
-			if alerts:
-				alert = alerts[0]
-				event = AlertEvent("weather_alert", alert["event"], f"{_paragraph_fix(alert['description'])}", self.refresh_minutes * 60 * 1000)
-				self.pm.add_event(event)
+		self.response = self.api.fetch()
+		self.set_timeout(self.config.update_mins * 60 * 1000)
+		if self.response.get("error"):
+			print(f"Error fetching data: {self.response['error']}")
+			return False
 		return True
 
 	def onEvent(self, event):
