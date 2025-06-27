ae47377dd78cf42de0d1417d9f6005d5f2cf45d1
Gregory Smith <greg@agilefrontiers.com>
2025-06-26 20:32:50 -0400

    update

--- modules/web_api.py
+++ modules/web_api.py
@@ -81,9 +81,9 @@ class WebApi(PMModule):
 		if not force and not self.display_timer.is_timedout(): return False
 		self.clear_region()
 		gfx = self.gfx
-		next_y0 = self._render_text(self.items[0]['header'], gfx.x0, gfx.y0, gfx.x1, gfx.y0, 1, self.config.fonts.header, halign="center", valign="top")
+		next_y0 = self._render_text(self.items[0]['header'], gfx.x0, gfx.y0, gfx.x1, gfx.y0, 2, self.config.fonts.header, halign="center", valign="top")
 		self._render_text(self.items[0]['body'], gfx.x0, next_y0, gfx.x1, gfx.y1, 0, self.config.fonts.body, halign="left", valign="top")
-		self._render_text(self.items[0]['footer'], gfx.x0, gfx.y1, gfx.x1, gfx.y1, -1, self.config.fonts.footer, halign="right", valign="bottom")
+		self._render_text(self.items[0]['footer'], gfx.x0, gfx.y1, gfx.x1, gfx.y1, -2, self.config.fonts.footer, halign="right", valign="bottom")
 		return True
 	
 	def exec(self) -> bool:
