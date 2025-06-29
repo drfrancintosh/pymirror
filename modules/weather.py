ba39d9f19dfc33c9579d3b0b5304337272bb9351
Gregory Smith <greg@agilefrontiers.com>
2025-06-29 02:45:45 -0400

    update

--- modules/weather.py
+++ modules/weather.py
@@ -55,7 +55,7 @@ class Weather(PMCard):
 
 	def exec(self) -> bool:
 		super().exec()
-		if not self.timer.is_timedout(): return False
+		if not self.timer.is_timedout(): return True
 		self.timer.set_timeout(self.refresh_minutes * 60 * 1000)
 		self.weather_response = self.weather_api.fetch(self.weather_data)
 		print(f"Weather response: {self.weather_response}")  # Debugging output
