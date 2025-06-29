9f2ac964efe543bc2428ff83ea2b7dadae036ee4
Gregory Smith <greg@agilefrontiers.com>
2025-06-29 01:36:55 -0400

    update

--- pymirror/pmcard.py
+++ pymirror/pmcard.py
@@ -109,7 +109,7 @@ class PMCard(PMModule):
 		""" Check if the card has changed and needs to be re-rendered. """
 		is_dirty = False
 		card = self._card.body
-		if card.is_dirty():
+		if card.last_text and card.is_dirty():
 			if card.is_fading_out():
 				is_dirty = True
 			else:
