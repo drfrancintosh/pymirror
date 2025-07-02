173f73b3afa53d4f4c1c31efbc4cdbbf7e150892
Gregory Smith <greg@agilefrontiers.com>
2025-07-01 22:58:22 -0400

    update

--- src/pymirror/pymirror.py
+++ src/pymirror/pymirror.py
@@ -135,9 +135,9 @@ class PyMirror:
 		try:
 			self.screen.bitmap.clear()
 			while True:
+				self.force_render = self._clear_screen  # Force a full render on the next loop
 				if self._clear_screen:
 					self.screen.bitmap.clear()
-					self.force_render = True  # Force a full render on the next loop
 					self._clear_screen = self._clear_screen_again
 				self._read_server_queue() # read any new events from the server queue
 				events = self.new_events # get any new events from server or modules
