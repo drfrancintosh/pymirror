a97a0500cfcbcfac9463c793b24cf6e37c58d5f1
Gregory Smith <greg@agilefrontiers.com>
2025-07-08 11:58:06 -0400

    update

--- run.sh
+++ run.sh
@@ -6,6 +6,4 @@ else
     CONFIG="./configs/$1"
 fi
 
-# Create a new process group and redirect everything
-exec > src/pmserver/static/output.log 2>&1
-PYTHONPATH=src python3 -u -m pymirror.pymirror --config "$CONFIG"
+PYTHONPATH=src python3 -u -m pymirror.pymirror --config "$CONFIG" > src/pmserver/static/output.log 2>&1
