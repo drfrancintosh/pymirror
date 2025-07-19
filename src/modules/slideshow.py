005babb79826c5554089b5e50cfbaed361214965
Gregory Smith <greg@agilefrontiers.com>
2025-07-18 23:22:26 -0400

    fixing images

--- src/modules/slideshow.py
+++ src/modules/slideshow.py
@@ -1,3 +1,4 @@
+from random import random, randint, choice  # Import the functions you need
 from modules.image import Image
 from pymirror.pmmodule import PMModule
 from pymirror.pmtimer import PMTimer
@@ -10,6 +11,7 @@ class Slideshow(Image):
 		super().__init__(pm, config)
 		self._image = config.slideshow ## very cheap way to reuse the Image module
 		self._slideshow = config.slideshow
+		self.randomize = self._slideshow.randomize
 		self.folder = self._slideshow.folder
 		self.images = self.load_folder(self.folder)
 		self.timer = PMTimer(1)
@@ -27,9 +29,12 @@ class Slideshow(Image):
 	def exec(self):
 		if self.timer.is_timedout():
 			self.timer.set_timeout(self._slideshow.interval_secs * 1000)  # Convert seconds to milliseconds
+			if self.randomize:
+				self.image_number = randint(0, len(self.images) - 1)
+			else:
+				self.image_number = (self.image_number + 1) % len(self.images)
 			self._image.path = os.path.join(self.folder, self.images[self.image_number])
 			self.image = self.load(self._image.path)
-			self.image_number = (self.image_number + 1) % len(self.images)
 			self.dirty = True
 		return self.dirty
 
