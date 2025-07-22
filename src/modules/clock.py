284c7eda8374250c048432582caac1ba8be89b34
Gregory Smith <greg@agilefrontiers.com>
2025-07-22 07:40:27 -0400

    refactoring config files

--- src/modules/clock.py
+++ src/modules/clock.py
@@ -24,19 +24,38 @@ class Clock(PMModule):
 	def exec(self) -> bool:
 		# if date_format includes "%W" or "%U", update the week number
 		# all this ceremony because %W and %U are zero-based and we must add 1 to them
-		if self.date_format and "%W" in self.date_format:
-			self.date_format = self.date_format.replace("%W", "_W_")
-		if self.date_format and "%U" in self.date_format:
-			self.date_format = self.date_format.replace("%U", "_U_")
-		self.curr_time = datetime.now().strftime(self.date_format)
-		if self.date_format and "_W_" in self.date_format:
+		to_lower = False
+		to_upper = False
+		date_format = self.date_format
+		if not date_format: return False
+		if "%W" in date_format:
+			date_format = date_format.replace("%W", "_W_")
+		if "%U" in date_format:
+			date_format = date_format.replace("%U", "_U_")
+
+		# lower case the date format if it starts with "-"
+		if date_format[0] == "-":
+			date_format = date_format[1:]
+			to_lower = True
+		if date_format[0] == "+":
+			date_format = date_format[1:]
+			to_upper = True
+
+		self.curr_time = datetime.now().strftime(date_format)
+
+		if "_W_" in date_format:
 			week_number = int(datetime.now().strftime("%W")) + 1
 			self.curr_time = self.curr_time.replace("_W_", f"{week_number:02d}")
-		if self.date_format and "_U_" in self.date_format:
+		if "_U_" in date_format:
 			week_number = int(datetime.now().strftime("%U")) + 1
 			self.curr_time = self.curr_time.replace("_U_", f"{week_number:02d}")
-		return self.curr_time != self.last_time 
-	
+		if to_lower:
+			self.curr_time = self.curr_time.lower()
+		if to_upper:
+			self.curr_time = self.curr_time.upper()
+
+		return self.curr_time != self.last_time
+
 	def onEvent(self, event):
 		pass
 
